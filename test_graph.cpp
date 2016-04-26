96417c1deaca08f52b8ababc6076f1545df6bb4f
jcguy <rhetorical575@tamu.edu>
2016-04-26 13:03:15 -0500

    Changed indentations to spaces

--- test_graph.cpp
+++ test_graph.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <map>
 
 #include "graph.h"
 #include "graph_algorithms.h"
@@ -8,57 +9,62 @@
 using namespace std;
 
 int main() {
-  timer t;
-  t.start();
+    timer t;
+    t.start();
 
-  // Create a graph.
-  graph<int, double> g;
-  // g.insert_vertex(5);
-  // g.insert_vertex(4);
-  // g.insert_edge(0, 1, 0.5);
-  //  g.insert_edge(1, 0, 0.25);
+    // Create a graph.
+    graph<int, double> g;
+    // g.insert_vertex(5);
+    // g.insert_vertex(4);
+    // g.insert_edge(0, 1, 0.5);
+    //  g.insert_edge(1, 0, 0.25);
 
-  // testing input stream
-  ifstream is{"football.g"};
-  is>>g;
+    // testing input stream
+    ifstream is{"football.g"};
+    is>>g;
 
-  t.stop();
-  cout << "Creating the graph took " << t.elapsed() / 1e6 << " ms" << endl;
-  t.restart();
+    t.stop();
+    cout << "Creating the graph took " << t.elapsed() / 1e6 << " ms" << endl;
+    t.restart();
 
 
-  cout << "Number of vertices: " << g.num_vertices() << endl;
-  cout<< "Number of edges: " << g.num_edges() << endl;
-  // Exercise iterators and accessors.
-  /* for(auto vi = g.vertices_begin(); vi != g.vertices_end(); ++vi)
-    cout << (*vi).second->descriptor() << " " << (*vi).second->property() << endl;
+    cout << "Number of vertices: " << g.num_vertices() << endl;
+    cout<< "Number of edges: " << g.num_edges() << endl;
+    // Exercise iterators and accessors.
+    /* for(auto vi = g.vertices_begin(); vi != g.vertices_end(); ++vi)
+        cout << (*vi).second->descriptor() << " " << (*vi).second->property() << endl;
 
-  for(auto vi = g.vertices_cbegin(); vi != g.vertices_cend(); ++vi)
-    cout << (*vi).second->descriptor() << " " << (*vi).second->property() << endl;
+    for(auto vi = g.vertices_cbegin(); vi != g.vertices_cend(); ++vi)
+        cout << (*vi).second->descriptor() << " " << (*vi).second->property() << endl;
 
-  for(auto ei = g.edges_begin(); ei != g.edges_end(); ++ei)
-    cout << (*ei).second->source() << " " << (*ei).second->target() << " "
-         << (*ei).second->property() << endl;
+    for(auto ei = g.edges_begin(); ei != g.edges_end(); ++ei)
+        cout << (*ei).second->source() << " " << (*ei).second->target() << " "
+                 << (*ei).second->property() << endl;
 
-  for(auto ei = g.edges_cbegin(); ei != g.edges_cend(); ++ei)
-    cout << (*ei).second->source() << " " << (*ei).second->target() << " "
-         << (*ei).second->property() << endl;
-  */
-  // Exercise find
-  //  graph<int, double>::edge_descriptor ed(0, 1);
-  //cout << (*g.find_vertex(0)).second->property() << " "
-  //     << (*g.find_edge(ed)).second->property()
-  //     << endl;
+    for(auto ei = g.edges_cbegin(); ei != g.edges_cend(); ++ei)
+        cout << (*ei).second->source() << " " << (*ei).second->target() << " "
+                 << (*ei).second->property() << endl;
+    */
+    // Exercise find
+    //  graph<int, double>::edge_descriptor ed(0, 1);
+    //cout << (*g.find_vertex(0)).second->property() << " "
+    //     << (*g.find_edge(ed)).second->property()
+    //     << endl;
 
-  // Exercise erase
-  //  g.erase_edge(ed);
-  // g.erase_vertex(0);
+    // Exercise erase
+    //  g.erase_edge(ed);
+    // g.erase_vertex(0);
 
-  // Exercise output
-  cout << "output" << endl;
-  cout << g;
+    // Exercise output
+    cout << "output" << endl;
+    cout << g;
 
-  t.stop();
-  cout << "Exercises took " << t.elapsed() / 1e6 << " ms" << endl;
+    t.stop();
+    cout << "Exercises took " << t.elapsed() / 1e6 << " ms" << endl;
+
+    map<size_t, size_t> p;
+    graph<int, double>::vertex_descriptor vd(1);
+
+    breadth_first_search(g, vd ,p);
 
 }
